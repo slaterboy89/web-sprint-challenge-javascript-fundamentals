0fd3cbe1e99d285629c9138eef9b67483fe1a46c
Leonard <slaterboy89@gmail.com>
2020-06-19 12:55:14 -0500

    completed prototypes.js

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,15 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: nestedFunction is basically a child of the the parent myFunction which means that it can inherit from the parent but the parent can't inherit from child.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(num){
+  let sum = 0;
+  return function final(){
+    return sum++;
+  }
+}
